ec9f24c4225bc15b73c0bfb7db33b3e8258156f1
Robert-J-WANG <robin.nz420@gmail.com>
2023-08-09 18:31:50 +1200

    update comments

--- src/components/锚点导航双向绑定/AnchorDemo.jsx
+++ src/components/锚点导航双向绑定/AnchorDemo.jsx
@@ -3,40 +3,43 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { FloorItems } from './data'
 
 export default function AnchorDemo () {
-  const navigate = useNavigate()
-  const location = useLocation()
-  const [offsetTopList, setOffsetTopList] = useState([])
-  const [activeBar, setActiveBar] = useState(FloorItems[0].key)
+  const navigate = useNavigate() // 用于在React Router中进行页面导航
+  const location = useLocation() // 获取当前页面的URL信息
+  const [offsetTopList, setOffsetTopList] = useState([]) // 保存每个锚点位置的信息
+  const [activeBar, setActiveBar] = useState(FloorItems[0].key) // 用于标记当前活动的锚点
 
   useEffect(() => {
     const offsetTop = []
     FloorItems.forEach(item => {
-      const curDom = document.getElementById(item.key)
+      const curDom = document.getElementById(item.key) // 获取每个锚点元素的DOM节点
       if (curDom) {
         offsetTop.push({
           id: item.key,
-          offsetTop: curDom.getBoundingClientRect().top
+          offsetTop: curDom.getBoundingClientRect().top // 获取锚点元素相对于视口顶部的位置
         })
       }
     })
-    setOffsetTopList(offsetTop)
+    setOffsetTopList(offsetTop) // 保存锚点位置信息到状态中
   }, [])
 
-  const ref = useRef(null)
+  const ref = useRef(null) // 创建一个引用，用于获取滚动容器的DOM节点
 
   const onScrollEvent = () => {
     if (offsetTopList && ref.current) {
       for (let i = offsetTopList.length - 1; i >= 0; i--) {
         if (ref.current.scrollTop + 400 >= offsetTopList[i].offsetTop) {
-          setActiveBar(offsetTopList[i].id)
+          setActiveBar(offsetTopList[i].id) // 更新活动的锚点状态
+
+          // 构建新的URL路径，用于导航
           const newPath = `${location.pathname}${location.search}`
           navigate(offsetTopList[i].id, { replace: true, state: { newPath } })
 
+          // 获取对应的锚点链接元素并修改其样式
           const anchorElement = document.querySelector(
             `a[href="#${offsetTopList[i].id}"]`
           )
           if (anchorElement) {
-            anchorElement.style.color = 'red' // Modify anchor's attribute
+            anchorElement.style.color = 'red' // 修改锚点链接的颜色属性
           }
           break
         }
@@ -49,8 +52,9 @@ export default function AnchorDemo () {
     const anchorElement = document.getElementById(item.key)
     if (anchorElement) {
       const offsetTop = anchorElement.getBoundingClientRect().top
-      anchorElement.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      anchorElement.scrollIntoView({ behavior: 'smooth', block: 'start' }) // 平滑滚动到锚点位置
 
+      // 构建新的URL路径，用于导航
       const newPath = `${location.pathname}${location.search}`
       navigate(item.key, { replace: true, state: { newPath } })
     }
@@ -64,8 +68,8 @@ export default function AnchorDemo () {
           scrollBehavior: 'smooth',
           height: '100vh'
         }}
-        onScrollCapture={onScrollEvent}
-        ref={ref}
+        onScrollCapture={onScrollEvent} // 监听滚动事件并执行onScrollEvent函数
+        ref={ref} // 将引用绑定到滚动容器的DOM节点
       >
         {FloorItems.map(item => (
           <div
@@ -102,9 +106,9 @@ export default function AnchorDemo () {
               lineHeight: '50px',
               textAlign: 'center',
               textDecoration: 'none',
-              color: activeBar === item.key ? 'red' : 'black'
+              color: activeBar === item.key ? 'red' : 'black' // 根据活动状态设置锚点链接颜色
             }}
-            onClick={handleClick(item)}
+            onClick={handleClick(item)} // 点击锚点链接时触发handleClick函数
           >
             {item.content}
           </a>
